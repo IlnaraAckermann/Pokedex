178cead3f554de1d2215dbdfe389994735823ef8
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-04-26 15:24:59 -0300

    Card Component

--- src/js/main.js
+++ src/js/main.js
@@ -3,39 +3,36 @@ const loadMoreButton = document.getElementById("loadMoreButton");
 const limit = 20;
 let offset = 0;
 
-// const count = fetch('https://pokeapi.co/api/v2/pokemon').then((response) => response.json()).then((jsonBody) =>{Number(jsonBody.count); return count;})
-// console.log(count)
+
+
 
 function loadMorePokemons(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons
       .map(
-        (pokemon) => 
-          `<li class="pokemon ${pokemon.mainType}">
-      <span class="number">#${pokemon.numberID}</span>
-      <span class="name">${pokemon.name}</span>
-      
-      <div class="details">
-          <ol class="types">
-            ${pokemon.types
-              .map((type) => `<li class="type ${type}">${type}</li>`)
-              .join("")}
-          </ol>
-          <img src="${pokemon.imagem}" alt="Foto do Pokemon ${
-            pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-          }">
-      </div>
-  </li>
-  `
+        (pokemon) =>
+          `
+          <pokemon-card
+          mainType="${pokemon.mainType}"
+          pokemon-number="${pokemon.numberID}"
+          pokemon-name="${pokemon.name}"
+          pokemon-src='${pokemon.imagem}'
+          pokemon-types='${pokemon.types
+            .map((type) => `<li class="type ${type}">${type}</li>`)
+            .join("")}'
+            ></pokemon-card>
+          `
       )
-      .join("");
+      .join('');
     pokemonsList.innerHTML += newHtml;
   });
 }
 
 loadMoreButton.addEventListener("click", () => {
-  offset += limit
+  offset += limit;
   loadMorePokemons(offset, limit);
 });
 
 loadMorePokemons(offset, limit);
+
+
