a882c4def6cadfeef93cb99d7fd620af0ccb3967
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-05-12 08:17:12 -0300

    seach add

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
 const pokemonsList = document.getElementById("pokemonsList");
 const loadMoreButton = document.getElementById("loadMoreButton");
+const previousButton = document.getElementById("previousButton");
+const nextButton = document.getElementById("nextButton");
 const limit = 20;
 let offset = 0;
+let count = 0;
 
+pokeApi
+  .getCount()
+  .then(() => {
+    count = pokeApi.count;
+    loadMorePokemons(offset, limit);
 
-
-
-function loadMorePokemons(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHtml = pokemons
-      .map(
-        (pokemon) =>
-          `
+    function loadMorePokemons(offset, limit) {
+      pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons
+          .map(
+            (pokemon) =>
+              `
           <pokemon-card
           mainType="${pokemon.mainType}"
           pokemon-number="${pokemon.numberID}"
@@ -22,17 +28,82 @@ function loadMorePokemons(offset, limit) {
             .join("")}'
             ></pokemon-card>
           `
-      )
-      .join('');
-    pokemonsList.innerHTML += newHtml;
-  });
-}
+          )
+          .join("");
+        pokemonsList.innerHTML = newHtml;
+      });
+    }
 
-loadMoreButton.addEventListener("click", () => {
-  offset += limit;
-  loadMorePokemons(offset, limit);
-});
+    nextButton.addEventListener("click", () => {
+      if (offset + limit < count - limit) {
+        offset += limit;
+        loadMorePokemons(offset, limit);
+      } else if (offset != count - limit) {
+        offset = count - limit;
+        loadMorePokemons(offset, limit);
+      }
+    });
 
-loadMorePokemons(offset, limit);
+    previousButton.addEventListener("click", () => {
+      if (offset >= limit) {
+        offset -= limit;
+        loadMorePokemons(offset, limit);
+      } else if (offset != 0) {
+        offset = 0;
+        loadMorePokemons(offset, limit);
+      }
+    });
+   
+    // searchButton.addEventListener("click", () => {
+    //   const searchValue = searchInput.value.toLowerCase();
+    //   const searchUrl = `https://pokeapi.co/api/v2/pokemon/${searchValue}`;
+    //   fetch(searchUrl)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       const pokemon = convertePokeApiDetailToPokemon(data);
+    //       const newHtml = `
+    //         <pokemon-card
+    //         mainType="${pokemon.mainType}"
+    //         pokemon-number="${pokemon.numberID}"
+    //         pokemon-name="${pokemon.name}"
+    //         pokemon-src='${pokemon.imagem}'
+    //         pokemon-types='${pokemon.types
+    //           .map((type) => `<li class="type ${type}">${type}</li>`)
+    //           .join("")}'
+    //           ></pokemon-card>
+    //         `;
+    //       pokemonsList.innerHTML = newHtml;
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //       pokemonsList.innerHTML = `<p>No Pokémon found for "${searchValue}"</p>`;
+    //     });
+    // });
+      
 
 
+    
+
+  searchButton.addEventListener("click", () => {
+      const searchValue = searchInput.value.toLowerCase();
+      pokeApi.searchPokemon(searchValue).then((pokemon) => {
+    const newHtml = `
+            <pokemon-card
+            mainType="${pokemon.mainType}"
+            pokemon-number="${pokemon.numberID}"
+            pokemon-name="${pokemon.name}"
+            pokemon-src='${pokemon.imagem}'
+            pokemon-types='${pokemon.types
+              .map((type) => `<li class="type ${type}">${type}</li>`)
+              .join("")}'
+              ></pokemon-card>
+            `;
+  pokemonsList.innerHTML = newHtml;
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+  })
+
+
+  }).catch((error) => console.log(error));
